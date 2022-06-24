775db6c1d2502d6c49619c24a9506022fe21ebb4
DiegoAngelino <diego.angeino.91@gmail.com>
2022-06-24 19:02:42 +0200

    Svelte w/ TS and Material UI setup

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<svelte:head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
+</svelte:head>
+
+<script lang="ts">
+	export let name: string;
+	import { Card } from 'sveltestrap';
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Card body color="primary" inverse class="mb-3">Primary</Card>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
